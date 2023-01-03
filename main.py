450cd0b441ca8d1a4a8a05e56c0936cb922ea601
rafalosa <rafal.osad@gmail.com>
2023-01-03 10:55:52 +0100

    GPU device picking for server computation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import numpy as np
 import tensorflow as tf
-from keras.optimizers import Adam
 import matplotlib.pyplot as plt
 from preprocessing import dataset, image_preparation, train_test_load
 from utils import TrainLogger
@@ -12,10 +11,6 @@ import os
 
 
 if __name__ == '__main__':
-
-    target = "dataset"
-    dataset.get_maps_dataset(target_directory=target, verbose=True)
-    image_preparation.split_and_divide(dataset_path="dataset/maps", processed_path="processed", final_image_divisor=2)
 
     parser = argparse.ArgumentParser(description='CycleGAN training script.')
 
@@ -32,11 +27,24 @@ if __name__ == '__main__':
                         help="directory for storing model checkpoints. If not empty, the script will attempt to restore"
                              "last saved checkpoint.")
 
+    parser.add_argument("--gpu", type=int, default=None,
+                        help="GPU to be used for training.")
+
     args = parser.parse_args()
     epochs = args.epochs
     split = args.split
     seed = args.seed
     dir_candidate = args.training_dir
+    gpu = args.gpu
+
+    if gpu is None:
+        raise RuntimeError("Please specify GPU device using the --gpu argument.")
+    else:
+        gpu = f"/device:GPU:{gpu}"
+
+    target = "dataset"
+    dataset.get_maps_dataset(target_directory=target, verbose=True)
+    image_preparation.split_and_divide(dataset_path="dataset/maps", processed_path="processed", final_image_divisor=2)
 
     if dir_candidate is not None:
         training_dir_name = dir_candidate
@@ -45,35 +53,37 @@ if __name__ == '__main__':
         os.mkdir(training_dir_name)
         print("Created new training directory.")
 
-    model = CycleGAN(input_dim=256, seed=seed)
+    try:
+        with tf.device(gpu):
 
-    model.compile(input_domain_discriminator_optimizer=Adam(2e-4, beta_1=0.5),
-                  i2t_generator_optimizer=Adam(2e-4, beta_1=0.5),
-                  target_domain_discriminator_optimizer=Adam(2e-4, beta_1=0.5),
-                  t2i_generator_optimizer=Adam(2e-4, beta_1=0.5))
+            model = CycleGAN(input_dim=256, seed=seed)
 
-    checkpoint = tf.train.Checkpoint(cycle_gan=model)
+            model.compile(input_domain_discriminator_optimizer=tf.keras.optimizers.Adam(2e-4, beta_1=0.5),
+                          i2t_generator_optimizer=tf.keras.optimizers.Adam(2e-4, beta_1=0.5),
+                          target_domain_discriminator_optimizer=tf.keras.optimizers.Adam(2e-4, beta_1=0.5),
+                          t2i_generator_optimizer=tf.keras.optimizers.Adam(2e-4, beta_1=0.5))
 
-    checkpoint_manager = tf.train.CheckpointManager(checkpoint, training_dir_name, max_to_keep=3)
+            checkpoint = tf.train.Checkpoint(cycle_gan=model)
 
-    if checkpoint_manager.latest_checkpoint:
-        checkpoint.restore(checkpoint_manager.latest_checkpoint)
-        print("Restored last model checkpoint.")
+            checkpoint_manager = tf.train.CheckpointManager(checkpoint, training_dir_name, max_to_keep=3)
 
-    (train_input_dataset, test_input_dataset, val_input_dataset, train_target_dataset, test_target_dataset,
-     val_target_dataset) = train_test_load(input_img_dir="processed/300x300/satellite",
-                                           target_img_dir="processed/300x300/maps",
-                                           val_test_size=split, paired=False, augmentation=False)
+            if checkpoint_manager.latest_checkpoint:
+                checkpoint.restore(checkpoint_manager.latest_checkpoint)
+                print("Restored last model checkpoint.")
 
-    training_dataset = tf.data.Dataset.zip((train_input_dataset, train_target_dataset))
-    test_dataset = tf.data.Dataset.zip((test_input_dataset, test_target_dataset))
-    val_dataset = tf.data.Dataset.zip((val_input_dataset, val_target_dataset))
+            (train_input_dataset, test_input_dataset, val_input_dataset, train_target_dataset, test_target_dataset,
+             val_target_dataset) = train_test_load(input_img_dir="processed/300x300/satellite",
+                                                   target_img_dir="processed/300x300/maps",
+                                                   val_test_size=split, paired=False, augmentation=False)
 
-    history = model.fit(training_dataset, epochs=epochs,
-                        batch_size=8,
-                        verbose=0,
-                        callbacks=[TrainLogger(checkpoint_manager=checkpoint_manager, save_every=5)],
-                        validation_data=val_dataset)
+            training_dataset = tf.data.Dataset.zip((train_input_dataset, train_target_dataset))
+            test_dataset = tf.data.Dataset.zip((test_input_dataset, test_target_dataset))
+            val_dataset = tf.data.Dataset.zip((val_input_dataset, val_target_dataset))
 
-
-
+            history = model.fit(training_dataset, epochs=epochs,
+                                batch_size=8,
+                                verbose=0,
+                                callbacks=[TrainLogger(checkpoint_manager=checkpoint_manager, save_every=5)],
+                                validation_data=val_dataset)
+    except RuntimeError as e:
+        print(e)
